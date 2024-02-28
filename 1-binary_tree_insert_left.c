ff930da4cd437e155f31225c7fc7cb2ca910128d
SH-Codes <wldrms@outlook.com>
2024-02-28 06:19:07 +0200

    1-left - inserts a node as the left-child of another node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,27 +15,21 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	/*Check if parent is NULL*/
+	binary_tree_t *new;
+
 	if (parent == NULL)
 		return (NULL);
 
-	/*Create a new node with the given value*/
-
-	binary_tree_t *new_node = binary_tree_node(parent, value);
-
-	if (new_node == NULL)
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
 		return (NULL);
 
-	/*If parent already has a left-child, set it as left child of new node*/
 	if (parent->left != NULL)
 	{
-		new_node->left = parent->left;
-		parent->left->parent = new_node;
+		new->left = parent->left;
+		parent->left->parent = new;
 	}
+	parent->left = new;
 
-	/*Set the new node as the left child of the parent*/
-	parent->left = new_node;
-
-	/*Return the new node*/
-	return (new_node);
+	return (new);
 }
