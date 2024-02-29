bab9a9793a9d838384aa8c5f9e7eeeb84b548b19
SH-Codes <wldrms@outlook.com>
2024-02-29 22:37:15 +0200

    2-right -  inserts a node as the right-child of another node

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -3,32 +3,35 @@
 /**
  * binary_tree_insert_right - Insert a node as the right-child
  *                            of another in a binary tree.
- * @parent: A pointer to the node to insert the right-child in.
+ * @node: A pointer to the node to insert the right-child in.
  * @value: The value to store in the new node.
  *
- * Return: If parent is NULL or an error occurs - NULL.
+ * Return: If node is NULL or an error occurs - NULL.
  *         Otherwise - a pointer to the new node.
  *
- * Description: If parent already has a right-child, the new node
+ * Description: If node already has a right-child, the new node
  *              takes its place and the old right-child is set as
  *              the right-child of the new node.
  */
-
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *node, int value)
 {
-	if (parent == NULL)
+	binary_tree_t *new_node;
+
+	if (node == NULL)
 		return (NULL);
 
-	binary_tree_t *new_node = binary_tree_node(parent, value);
+	new_node = binary_tree_node(node, value);
 
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->right = parent->right;
-	parent->right = new_node;
+	if (node->right != NULL)
+	{
+		new_node->right = node->right;
+		node->right->parent = new_node;
+	}
 
-	if (new_node->right != NULL)
-		new_node->right->parent = new_node;
-
+	node->right = new_node;
 	return (new_node);
 }
+
