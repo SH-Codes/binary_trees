676d03516ec5d9be92805663e47f5de3306643ea
SH-Codes <wldrms@outlook.com>
2024-03-01 00:17:39 +0200

    13-nodes - counts the nodes with at least 1 child in a binary tree

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,20 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - Counts the nodes with at least 
- * 1 child in a binary tree.
- * @tree: A pointer to the root node of the tree to 
- * count the number of nodes.
+ * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree.
+ * @tree: A pointer to the root node of the tree to count the number of nodes.
  *
- * Return: If tree is NULL, the function must return 0,
- * else return node count.
+ * Return: If tree is NULL, the function must return 0, else return node count.
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return 0;
-
-    return (tree->left || tree->right) +
-           binary_tree_nodes(tree->left) +
-           binary_tree_nodes(tree->right);
+	if (tree == NULL)
+		return (0);
+	size_t has_child = (tree->left != NULL || tree->right != NULL);
+	return (has_child + binary_tree_nodes(tree->left) +
+			binary_tree_nodes(tree->right));
 }
