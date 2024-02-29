485e159b2131e19c7cb958243682303a1105fe5d
SH-Codes <wldrms@outlook.com>
2024-02-29 23:35:34 +0200

    9-height - measures the height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,18 +3,21 @@
 /**
  * binary_tree_height - Measures the height of a binary tree.
  * @tree: A pointer to the root node of the tree to measure
- * the height.
+* the height.
  *
- * Return: If tree is NULL, your function must return 0,
- * else return the height.
+ * Return: If tree is NULL, return 0, else return the height.
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+	if (tree)
+	{
+		size_t left_height = 0, right_height = 0;
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+		left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-	return (left_height > right_height ? left_height + 1 : right_height + 1);
+		return ((left_height > right_height) ? left_height : right_height);
+	}
+	return (0);
 }
